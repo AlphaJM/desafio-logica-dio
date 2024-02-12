7a79daef6b52fd30435ad305e7b746641993b8e5
AlphaJM <jmpp13@outlook.com>
2024-02-12 10:09:36 -0300

    Segundo Commit

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Testando.")
+let nomeHeroi = "4LPha_Br"
+let xpHeroi = 6000
+let acunhaHeroi
+
+if(xpHeroi <= 1000){
+    acunhaHeroi = "Ferro"
+    console.log("O Herói de nome " + nomeHeroi + " está no nivel de " + acunhaHeroi);
+}
+else if(xpHeroi >= 1001 && xpHeroi <= 2000){
+    acunhaHeroi = "Bronze"
+    console.log("O Herói de nome " + nomeHeroi + " está no nivel de " + acunhaHeroi);
+}
+else if(xpHeroi >= 2001 && xpHeroi <= 5000){
+    acunhaHeroi = "Prata"
+    console.log("O Herói de nome " + nomeHeroi + " está no nivel de " + acunhaHeroi);
+}
+else if(xpHeroi >= 5001 && xpHeroi <= 7000){
+    acunhaHeroi = "Ouro"
+    console.log("O Herói de nome " + nomeHeroi + " está no nivel de " + acunhaHeroi);
+}
+else if(xpHeroi >= 7001 && xpHeroi <= 8000){
+    acunhaHeroi = "Platina"
+    console.log("O Herói de nome " + nomeHeroi + " está no nivel de " + acunhaHeroi);
+}
+else if(xpHeroi >= 8001 && xpHeroi <= 9000){
+    acunhaHeroi = "Ascedente"
+    console.log("O Herói de nome " + nomeHeroi + " está no nivel de " + acunhaHeroi);
+}
+else if(xpHeroi >= 9001 && xpHeroi <= 10000){
+    acunhaHeroi = "Imortal"
+    console.log("O Herói de nome " + nomeHeroi + " está no nivel de " + acunhaHeroi);
+}
+else{
+    acunhaHeroi = "Radiante"
+    console.log("O Herói de nome " + nomeHeroi + " está no nivel de " + acunhaHeroi);
+}
